8d74f9cc77a8d44ca8b649abb6aed7ba4ab05443
Ari Meles-Braverman <arimb1999@gmail.com>
2018-09-21 03:52:05 +0300

    fixes to event predictor

--- Event Predictor/EventPredictor2.py
+++ Event Predictor/EventPredictor2.py
@@ -1,7 +1,21 @@
-import re
+# import re
 import random
 import tkinter as tk
 from tkinter import filedialog
+import requests
+
+def getdata(url):
+    try:
+        ans = requests.get("https://www.thebluealliance.com/api/v3/" + url,
+                           "accept=application%2Fjson&X-TBA-Auth-Key=gl4GXuoqG8anLUrLo356LIeeQZk15cfSoXF72YT3mYkI38cCoAmReoCSSF4XWccQ").json()
+        if ans is not None:
+            return ans
+        else:
+            print("oops null " + url)
+            getdata(url)
+    except:
+        print("oops " + url)
+        getdata(url)
 
 DPs = {}
 tk.Tk().withdraw()
@@ -14,20 +28,32 @@ for team in allteams[1:]:
     for i, element in enumerate(team[1:], start=1):
         DPs["frc" + team[0]][titles[i]] = float(element)
 
-teams = [tmp if tmp[:3]=="frc" else ("frc"+tmp) for tmp in (re.split(' ', input("Teams: ")))]
+# teams = [tmp if tmp[:3]=="frc" else ("frc"+tmp) for tmp in (re.split(' ', input("Teams: ")))]
+teams = getdata("event/"+input("event: ")+"/teams/keys")
+matches = int(input("Matches per Team: "))
 runs = int(input("Iterations: "))
 
 results = {team:[0,0] for team in teams}
-for i in range(0, runs):
+for i in range(runs):
     print(i)
-    random.shuffle(teams)
-    winner = sum([DPs[team]["Adj DP"] for team in teams[0:3]]) > sum([DPs[team]["Adj Qual DP"] if team in DPs else 25 for team in teams[3:6]])
-    for team in teams[0:6]:
-        results[team][1] += 1
-    for team in teams[0:3] if winner else teams[3:6]:
-        results[team][0] += 1
+    ev = {team:[0,0] for team in teams}
+    for j in range(int(len(teams)*matches/6)):
+        random.shuffle(teams)
+        winner = sum([DPs[team]["Adj Qual DP"] for team in teams[0:3]]) > sum([DPs[team]["Adj Qual DP"] for team in teams[3:6]])
+        for team in teams[0:6]:
+            ev[team][1] += 1
+        for team in teams[0:3] if winner else teams[3:6]:
+            ev[team][0] += 1
+    ev = sorted([(ev[tmp][0]/ev[tmp][1], tmp) for tmp in ev])
+    for rank, team in enumerate(ev, start=1):
+        print(type(team[1]))
+        print(team[1])
+        results[team[1]] += rank
 
-with open("results.csv", "w+") as file:
+for team in results:
+    results[team] /= runs
+
+with open("cc.csv", "w+") as file:
     file.write("Team,Wins,Plays,Avg\n")
     for team in results:
         file.write(team + "," + str(results[team][0]) + "," + str(results[team][1]) + "," + str(results[team][0]/results[team][1]) + "\n")

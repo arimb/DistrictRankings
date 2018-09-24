23b40a3d1d70ed87570bf6e5764e395281dae89b
Ari Meles-Braverman <arimb1999@gmail.com>
2018-09-24 15:43:18 +0300

    added win probability to event predictor

--- Event Predictor/EventPredictor2.py
+++ Event Predictor/EventPredictor2.py
@@ -1,4 +1,5 @@
 import random
+import math
 import requests
 
 def getdata(url):
@@ -15,7 +16,7 @@ def getdata(url):
         getdata(url)
 
 DPs = {}
-with open("../Ranking Points/world_RP.csv") as file:
+with open("DistrictRankings/Ranking Points/world_RP.csv") as file:
     allteams = file.readlines()
 titles = allteams[0].strip().split(",")
 for team in allteams[1:]:
@@ -28,7 +29,7 @@ for team in allteams[1:]:
 event = input("event: ")
 teams = getdata("event/"+event+"/teams/keys")
 num_matches = int(input("Matches per Team: "))
-with open("schedules/"+str(len(teams))+"_"+str(num_matches)+".csv") as file:
+with open("DistrictRankings/Event Predictor/schedules/"+str(len(teams))+"_"+str(num_matches)+".csv") as file:
     matches = file.readlines()
 matches = [[int(x) for x in match.strip().split(",")[0::2]] for match in matches]
 runs = int(input("Iterations: "))
@@ -41,29 +42,24 @@ for i in range(runs):
     ev = {team:[0,0] for team in teams}
     for match in matches:
         playing = [teams[x-1] for x in match]
-        redwins = sum([DPs[team]["Avg Win RP"] for team in playing[0:3]]) > sum([DPs[team]["Avg Win RP"] for team in playing[3:6]])
-        for team in playing[0:6]:
+        redwins = random.random() <= 1/(1+math.exp(-3.5*(sum([DPs[team]["Avg Win RP"] for team in playing[:3]])-sum([DPs[team]["Avg Win RP"] for team in playing[3:]]))))
+        for team in playing:
             ev[team][1] += 1
-        for team in playing[0:3] if redwins else playing[3:6]:
+        for team in playing[:3] if redwins else playing[3:]:
             ev[team][0] += 2
-        if 3*random.random() <= sum([DPs[team]["Avg Auto RP"] for team in playing[0:3]]):
-            for team in playing[0:3]:
-                ev[team][0] += 1
-        if 3*random.random() <= sum([DPs[team]["Avg Climb RP"] for team in playing[0:3]]):
-            for team in playing[0:3]:
-                ev[team][0] += 1
-        if 3*random.random() <= sum([DPs[team]["Avg Auto RP"] for team in playing[3:6]]):
-            for team in playing[3:6]:
-                ev[team][0] += 1
-        if 3*random.random() <= sum([DPs[team]["Avg Climb RP"] for team in playing[3:6]]):
-            for team in playing[3:6]:
-                ev[team][0] += 1
+        for alliance in [playing[:3], playing[3:]]:
+            if 3 * random.random() <= sum([DPs[team]["Avg Auto RP"] for team in alliance]):
+                for team in alliance:
+                    ev[team][0] += 1
+            if 3 * random.random() <= sum([DPs[team]["Avg Climb RP"] for team in alliance]):
+                for team in alliance:
+                    ev[team][0] += 1
     ev = sorted([(ev[tmp][0]/ev[tmp][1], tmp) for tmp in ev], reverse=True)
     for rank, team in enumerate(ev, start=1):
         results[team[1]][rank-1] += 1
         avg[team[1]] += rank
 
-with open(event+".csv", "w+") as file:
+with open("DistrictRankings/Event Predictor/"+event+".csv", "w+") as file:
     file.write("Team,Avg Rank,")
     for i in range(len(teams)):
         file.write(str(i+1) + ",")
